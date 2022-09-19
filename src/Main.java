f2363168395ec9ca34313c3780cdf05804ecc25a
EmadKazi-ux <111903215+EmadKazi-ux@users.noreply.github.com>
2022-09-19 11:56:18 -0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 14;
+
+        int modulo = number% 2;
+
+        System.out.println("since module of the number " + number + " is " + modulo + " the given number is even");
+
+
+
+
+    }
+}
